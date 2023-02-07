266bf1db3333cd50d9603c197ffe727955fefdf1
Prashant Kumar <iampears7@gmail.com>
2023-02-07 16:36:34 +0530

    ---

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,17 @@ import {
   NavigationBar,
   Login,
 } from "./components/Main/Index";
-// import { setCardHeight } from "./assets/script.js";
-// Main App Function Declaration
+
 function App() {
   // set card height equal
-  function setCardHeight() {
+  const setCardHeight= () => {
     const rs = document.querySelectorAll(".row-section");
     rs.forEach((r) => {
       const cards = r.querySelectorAll(".collapse .card");
       const cardHeight = Math.max(
         ...Array.from(cards).map((c) => c.clientHeight)
       );
-      // console.log(cardHeight);
+     
       cards.forEach((c) => {
         c.style.height = `${cardHeight}px`;
         c.style.maxHeight = "500px";
